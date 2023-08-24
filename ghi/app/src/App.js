edc12fda53427d12be778bab3c8c79435a5c31ce
Matt Baltzer <baltzer.matt@gmail.com>
2023-08-24 15:42:06 -0700

    Created AttendeesList, updated App.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,4 +1,5 @@
 import Nav from "./Nav.js";
+import AttendeesList from "./AttendeesList.js";
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -8,24 +9,7 @@ function App(props) {
     <>
       <Nav />
       <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Conference</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.attendees.map((attendee) => {
-              return (
-                <tr key={attendee.href}>
-                  <td>{attendee.name}</td>
-                  <td>{attendee.conference}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <AttendeesList attendees={props.attendees} />
       </div>
     </>
   );
